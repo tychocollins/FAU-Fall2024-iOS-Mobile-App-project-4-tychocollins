130c4e68602d6d6c0539305f2e91afee1818c005
tychocollins <tychocollins2020@fau.edu>
2024-10-04 15:46:28 -0400

    commit

---
+++ Flashcard/CreateFlashcardView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateFlashcardView.swift
+//  Flashcard
+//
+//  Created by Tycho Collins on 10/4/24.
+//
+
+import SwiftUI
+
+struct CreateFlashcardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateFlashcardView()
+}
