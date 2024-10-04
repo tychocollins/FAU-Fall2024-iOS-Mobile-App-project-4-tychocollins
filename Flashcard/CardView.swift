ea10a00cd4ac2d0a1d5a03abc99e98e65b955e66
tychocollins <tychocollins2020@fau.edu>
2024-10-04 14:51:45 -0400

    latest commit

---
+++ Flashcard/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Flashcard
+//
+//  Created by Tycho Collins on 10/4/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
